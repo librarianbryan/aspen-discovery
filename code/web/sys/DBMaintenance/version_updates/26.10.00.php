1b541dfa91dff47c15c948e82a8b70ccf405566f
librarianbryan <bryan.n.jones@nashville.gov>
2026-09-08 15:24:41 -0500

    DIS-2459 Add version update and release notes for 26.10.00

--- code/web/sys/DBMaintenance/version_updates/26.10.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.10.00.php
@@ -41,6 +41,16 @@ function getUpdates26_10_00(): array {
 
 		//jacob - OpenFifth
 
+		//bryan
+
+		 'add_simplified_search_box' => [
+                        'title' => 'Add Simplified Search Box setting',
+                        'description' => 'Adds a toggle to enable the simplified search box UI per library
+                        'continueOnError' => false,
+                        'sql' => [
+                                'ALTER TABLE library ADD COLUMN simplifiedSearchBox TINYINT(1) NOT NULL DE
+                        ]
+                ], //add_simplified_search_box
 
 	];
 }
